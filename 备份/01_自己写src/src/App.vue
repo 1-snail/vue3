874a6f9cb0bc8a5ace0d2ca44cb92a283ce115ff
dai <daijinlongyx@163.com>
2025-07-24 16:03:04 +0800

    学习vue3

---
+++ 备份/01_自己写src/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <!-- 写html -->
+     <div class="app">
+        <h1>你好啊</h1>
+     </div>
+</template>
+
+<script>
+    // 写 js 或 ts
+    export default{
+        name:"App" // 组件名
+    }
+</script>
+
+<style>
+/* 写样式 */
+.app{
+    background-color: red;
+    width: 200px;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+</style>
---
+++ 备份/01_自己写src/src/main.ts
@@ -0,0 +1,6 @@
+// 引入createApp 用于创建引用
+import { createApp } from "vue";
+// 引入 App 根组件
+import App from './App.vue'
+
+createApp(App).mount('#app')
