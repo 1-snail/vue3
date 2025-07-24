31997cf1c2c823e7e3e3a1955cdb4f0f2b165f70
dai <daijinlongyx@163.com>
2025-07-24 16:54:08 +0800

    增加component

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,18 @@
     <!-- 写html -->
      <div class="app">
         <h1>你好啊</h1>
+        <Preson/>
      </div>
 </template>
 
 <script>
+import Preson from './components/Preson.vue';
+import Person from './components/Preson.vue'
+
     // 写 js 或 ts
     export default{
-        name:"App" // 组件名
+        name:"App", // 组件名
+        components:{Person} // 注册组件
     }
 </script>
 
