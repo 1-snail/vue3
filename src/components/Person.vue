e3d21c3210e15a24a405c3f9136d35184c644ee3
snail <daijinlongyx@163.com>
2025-07-27 20:36:25 +0800

    增加watch

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,48 +1,54 @@
 <template>
    <div class="person">
-    <h1>情况二：监视【ref】监视的【对象类型】数据</h1>
-      <h2>姓名:{{ person.name }}</h2>
-      <h2>年龄:{{ person.age }}</h2>
-      <button @click="changeName">修改名称</button>
+      <h2>姓名：{{ person.name }}</h2>
+      <h2>年龄：{{ person.age }}</h2>
+      <h2>车辆信息</h2>
+      <ul>
+        <li v-for="p in person.car"  :key="p">{{ p }}</li>
+      </ul>
+<br>
+      <button @click="changeName">修改名字</button>
       <button @click="changeAge">修改年龄</button>
-      <button @click="changePerson">修改人</button>
+      <button @click="changeFirstCar">修改第一辆车</button>
+      <button @click="changeSecondCar">修改第二辆车</button>
+      <button @click="changeCar">修改车信息</button>
+
    </div>
 </template>
 
 
 <script lang="ts" setup>
-import { ref ,watch} from 'vue';
-
-  let person = ref( {name:"zhangsan",age :18})
+import { reactive } from 'vue';
 
 
-  function changeName(){
-    person.value.name += "~"
+let person = reactive({
+  name:'zhangsan',
+  age:18,
+  car:{
+    c1:'奔驰',
+    c2:'宝马'
   }
-  function changeAge (){
-    person.value.age ++
-    
+})
+
+ function changeName()
+  {
+    person.name += "~"
+  }
+  function  changeAge(){
+    person.age ++
+  }
+  function  changeFirstCar(){
+    person.car.c1 = '奥迪'
+  }
+  function  changeSecondCar(){
+    person.car.c2 = '大众'
+  }
+  function  changeCar(){
+    person.car = {c1:'雅迪',c2:"爱玛"}
   }
 
-function changePerson(){
-  person.value = {name:'李四',age:100}
-}
 
-/* 监视的是 person 的地址值，当地址值发生变化则触发回调函数
-若要监视 person 内部值，则需要配置watch第三个参数，加上 deep:true 
-immediate:true 表示先执行一次回调函数，无论person是否变化
-watch第一个参数：被监视的对象
-watch第二个参数：回调函数
-watch第三个参数：配置对象
-*/
-// 执行 changePerson() 是，newValue 和 oldValue值不同，
-// 但是当触发changeName()、changeAge() 函数时 newValue 和 oldValue值 相同
-watch(person,(newValue,oldValue)=>{
-  console.log(newValue,oldValue);
-  
-},{deep:true,immediate:true})
 
-   
 </script>
 
 <style scoped>
---
+++ 备份/15_监视reactivate定义的对象类型数据/src/components/Person.vue
@@ -0,0 +1,49 @@
+<template>
+   <div class="person">
+    <h1>情况三：监视【reactivate】监视的【对象类型】数据</h1>
+      <h2>姓名:{{ person.name }}</h2>
+      <h2>年龄:{{ person.age }}</h2>
+      <button @click="changeName">修改名称</button>
+      <button @click="changeAge">修改年龄</button>
+      <button @click="changePerson">修改人</button>
+   </div>
+</template>
+
+
+<script lang="ts" setup>
+import { reactive, toRefs, watch} from 'vue';
+
+  let person = reactive( {name:"zhangsan",age :18})
+
+
+  function changeName(){
+    person.name += "~"
+  }
+  function changeAge (){
+    person.age ++
+    
+  }
+
+function changePerson(){
+  Object.assign(person,{name:'李四',age:100})
+}
+
+// 不使用 deep:true ，reactivate 默认开启深度监视（隐式创建深层监听），无法关闭
+watch(person,(newValue,oldValue)=>{
+  console.log(newValue,oldValue);
+  
+})
+
+   
+</script>
+
+<style scoped>
+
+    .person{
+    margin: 20px;
+    background-color: skyblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+</style>
