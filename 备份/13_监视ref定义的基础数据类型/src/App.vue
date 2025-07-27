c65035f4476a913f7bd29c5db2e5d6380ba428cf
snail <daijinlongyx@163.com>
2025-07-27 17:09:24 +0800

    增加ref监视说明

---
+++ 备份/13_监视ref定义的基础数据类型/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- 写html -->
+    <Person/>
+</template>
+
+<script>
+import Person from './components/Person.vue';
+
+    // 写 js 或 ts
+    export default{
+        name:"App", // 组件名
+        components:{Person} // 注册组件
+    }
+</script>
+
+<style>
+
+</style>
---
+++ 备份/13_监视ref定义的基础数据类型/src/components/Person.vue
@@ -0,0 +1,43 @@
+<template>
+   <div class="person">
+    <h1>情况一：监视【ref】监视的【基本类型】数据</h1>
+      <h2>当前求和为：{{ sum }}</h2>
+      <button @click="changeSum">点我 sum + 1</button>
+   </div>
+</template>
+
+
+<script lang="ts" setup>
+import { ref ,watch} from 'vue';
+
+    let sum = ref(0)
+
+    function changeSum(){
+      sum.value ++
+    }
+
+   const stopWatch = watch(sum,(newValue,oldValue)=>{
+      console.log('sum发生变化',newValue,oldValue);
+      if(newValue>10){
+          stopWatch()
+      }
+    })
+
+
+    
+    // wathc(监视谁，回调函数)
+    // 回调函数 (newValue,oldValue) =>{ }
+
+   
+</script>
+
+<style scoped>
+
+    .person{
+    margin: 20px;
+    background-color: skyblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+</style>
