3f0459f4c93dfd88abf5d53ad8e8aaebd1446a53
dai <daijinlongyx@163.com>
2025-07-31 17:23:42 +0800

    完成router基本使用

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,69 @@
 <template>
-    <!-- 写html -->
-    <Person v-if="isShow"/>
+    <div class="app">
+        <h2 class="title">Vue路由测试</h2>
+        <!-- 导航去 -->
+         <div class="navigate">
+            <RouterLink active-class="active" to="/home">首页</RouterLink>
+            <RouterLink active-class="active" to="/news">新闻</RouterLink>
+            <RouterLink active-class="active" to="/about">关于</RouterLink>
+         </div>
+         <div class="main-content">
+            <RouterView></RouterView>
+         </div>
+    </div>
+   
 </template>
 
 <script lang="ts" setup name="App">
-
-import Person from './components/Person.vue';
-import {ref} from 'vue'
-let isShow = ref(true)
+import { RouterView,RouterLink } from 'vue-router';
 
 </script>
 
 <style>
+.title{
+    text-align: center;
+    word-spacing: 5px;
+    margin: 30px 0;
+    height: 70px;
+    line-height: 70px;
+    background-image: linear-gradient(54deg,gray,white);
+    border-radius: 10px;
+    box-shadow: 0 0 2px;
+    font-size: 30px;
+}
+.navigate{
+    display: flex;
+    justify-content: space-around;
+    margin: 0 100px;
+}
+
+.navigate a.active{
+    background-color: #64967E;
+    color: #ffc268;
+    font-weight: 900;
+    text-shadow: 0 0 1px black;
+    font-family: 微软雅黑;
+}
+.main-content{
+    margin: 0 auto;
+    margin-top: 30px;
+    border-radius: 10px;
+    width: 90%;
+    height: 400px;
+    border: 1px solid;
+}
+.navigate a{
+    display: block;
+    text-align: center;
+    width: 90px;
+    height:40px;
+    line-height: 40px;
+    border-radius: 10px;
+    background-color:gray;
+    text-decoration: none;
+    color: white;
+    font-size: 18px;
+    letter-spacing:5px;
+}
 
 </style>
---
+++ src/router/index.ts
@@ -0,0 +1,33 @@
+// 创建一个路由器，并暴露出去
+
+// 第一步：引入 createRouter
+import { createRouter ,createWebHistory} from "vue-router";
+
+// 引入一个个可能要呈现的组件
+import Home from "@/compoonents/Home.vue";
+import News from "@/compoonents/News.vue";
+import About from "@/compoonents/About.vue";
+
+// 第二部： 创建路由器
+
+const router = createRouter({
+    history:createWebHistory(),  // 路由器的工作模式
+
+    routes:[   // 一个一个的路由规则
+        {
+            path:'/home',
+            component:Home
+        },
+        {
+            path:'/news',
+            component:News
+        },
+        {
+            path:'/about',
+            component:About
+        }
+    ]
+})
+
+// 暴露 路由器
+export default router
