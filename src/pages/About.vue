93ad26f887c19ada498cd280d4ad8d6c62be6fd8
dai <daijinlongyx@163.com>
2025-08-01 13:28:31 +0800

    重构路由结构，添加Header组件，更新页面组件路径，移除无用组件

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -3,14 +3,11 @@
         <h2>大家好，欢迎来到尚硅谷直播间</h2>
     </div>
 </template>
-<script setup·lang="ts" ·name="About">
+<script setup ·lang="ts" ·name="About">
+
 </script>
 <style scoped>
-<<<<<<< HEAD
-.about {
-=======
 about {
->>>>>>> bcf18f7dc160289281f9a16253a12b7c9d44ab5d
     display: flex;
     justify-content: center;
     align-items: center;
