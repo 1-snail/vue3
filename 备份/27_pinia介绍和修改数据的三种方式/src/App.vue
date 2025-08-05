202d64125e4fb2aa9279273b6fbcff6719b2bab7
dai <daijinlongyx@163.com>
2025-08-05 09:56:21 +0800

    增加 Piaia的三种修改数据方式

---
+++ 备份/27_pinia介绍和修改数据的三种方式/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <Count/>  
+    <br></br>
+    <LoveTalk/>  
+</template>
+
+<script setup name="App" lang="ts">
+import Count from './components/Count.vue';
+import LoveTalk from './components/LoveTalk.vue';
+</script>
+
+<style scoped>
+</style>
